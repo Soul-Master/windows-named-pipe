7fde2cf0fc0fcb4c7a7ee17c3e652c8184b6d82e
Soul_Master <Soul_Master@live.com>
2017-08-21 09:06:04 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace ConsoleApp3
+{
+    /*
+     //./pipe/TestPipe
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new NamedPipeServerStream("TestPipe");
+            server.WaitForConnection();
+            var reader = new StreamReader(server);
+            var writer = new StreamWriter(server);
+
+            while (true)
+            {
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
